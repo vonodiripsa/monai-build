43eb641e3cdddb07d6db875655f10030a8b40ea7
Alexander Spiridonov <aspiridonov@yahoo.com>
2023-05-01 13:03:03 -0700

    clean

--- cli/components/triton/triton_tao/triton_tao_build_container/triton_build_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Getting the deployment repo"
-git clone https://github.com/NVIDIA-AI-IOT/tao-toolkit-triton-apps.git
-
-echo "Building the container"
-docker build -f "tao-toolkit-triton-apps/docker/Dockerfile" \
-             -t nvcr.io/nvidia/tao/triton-apps:22.06-py3 tao-toolkit-triton-apps
